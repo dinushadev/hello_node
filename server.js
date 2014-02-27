ccc64903c499eaec66dfe7b4d8afae0c0e0f2bda
dinusha <dnsw83@gmail.com>
2014-02-27 17:15:50 +0530

    change comment prot to 80

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var server = http.createServer(function (request, response) {
   response.end("Hello New Heruku World\n");
 });
 
+
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(80);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8777/");
+console.log("Server running at http://127.0.0.1:80/");
