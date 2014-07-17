2bee4cbaafccfe9dbdcaca3616e9f8f2e4e1cf40
dinusha <dnsw83@gmail.com>
2014-07-17 09:53:10 +0530

    add session and html render engin

--- server.js
+++ server.js
@@ -1,16 +1,77 @@
 // Load the http module to create an http server.
-var http = require('http');
+var http = require('http'),
+express = require('express'),
+ejs =require('ejs'),
+reqauest = require('request');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Pearson VPC2\n");
+
+
+var app = module.exports = express();
+
+
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
+
+// all environments
+app.set('port', process.env.PORT || 8081);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(__dirname + '/static/'));
+
+
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');  
+
+
+
+
+/**
+* Render the index page.
+*/
+app.get('/',  function index(request, response) {
+
+  response.render('index.html');
 });
 
 
-// Listen on port 8080, IP defaults to 127.0.0.1
-server.listen(8080);
+/**
+* Render the login page.
+*/
+app.post('/login',  function index(request, response) {
+  var name = request.body.name;
+  request.session.NAME= name;
+
+  response.redirect('home');
+});
+
+app.get('/logout',  function index(request, response) {
+ request.session.destroy(function(){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+  response.redirect('/');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+}); 
+});
+/**
+* Render the home page.
+*/
+app.get('/home',  function index(request, response) {
+
+ if(request.session.NAME){
+  response.render('home.html',{
+    name: request.session.NAME
+  });
+}else{
+  response.redirect('/');
+}
+
+});
 
 
-// Put a friendly message on the terminal
-console.log("Server running at 8080");
+
+// Configure our HTTP server to respond with Hello World to all requests.
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
+
